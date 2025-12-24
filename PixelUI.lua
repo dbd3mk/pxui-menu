79cb36011aa9396db8eb91d63243f9c45d85aed7
dbd3mk <kklw654321@gmail.com>
2025-12-24 10:08:08 +0300

    UI: Simplified menu to 4 main categories

--- PixelUI.lua
+++ PixelUI.lua
@@ -14,66 +14,21 @@ function PixelUI:Send(d) if DUI then MachoSendDuiMessage(DUI, json.encode(d)) en
 function PixelUI:Notify(t, ti, d) PixelUI:Send({ action="showNotification", type=t, title=ti, desc=d }) end
 
 function PixelUI:Build()
-    -- Tab: Actions
-    local actionsTab = {
-        { label = "Teleport to Waypoint", type = "button", onSelect = function() PixelUI:Notify("info", "TELEPORT", "Teleporting to waypoint...") end },
-        { label = "Heal & Armor", type = "button", onSelect = function() PixelUI:Notify("success", "SELF", "Stats Restored") end },
-        { label = "God Mode [OFF]", type = "toggle", onSelect = function() end },
-        { label = "Invisibility [OFF]", type = "toggle", onSelect = function() end },
-        { label = "Super Jump [OFF]", type = "toggle", onSelect = function() end },
-        { label = "Explode Nearest Vehicle", type = "button", danger = true, onSelect = function() end },
-        { label = "Ram Nearest Player", type = "button", danger = true, onSelect = function() end },
-    }
-
-    -- Tab: Players
-    local playersTab = {
-        { label = "Refresh Player List", type = "button", onSelect = function() PixelUI:UpdatePlayerTab() end },
-        -- Players will be injected here
-    }
-
-    -- Tab: Settings
-    local settingsTab = {
-        { label = "Menu Accent Color", type = "list", value = "Red", options = {"Red", "Blue", "Green", "Purple"} },
-        { label = "Discord Link", type = "button", onSelect = function() PixelUI:Notify("info", "DISCORD", "Link copied to clipboard") end },
-        { label = "Unload Menu", type = "button", danger = true, onSelect = function() end },
-    }
-
+    -- القائمة الرئيسية البسيطة كما طلب المستخدم
     ActiveMenu = {
-        tabs = {"Actions", "Players", "Settings"},
-        activeTabIdx = 0,
-        content = {
-            [0] = actionsTab,
-            [1] = playersTab,
-            [2] = settingsTab
-        }
+        { label = "SELF", type = "subMenu", subTabs = {} },
+        { label = "PLAYER", type = "subMenu", subTabs = {} },
+        { label = "SERVER", type = "subMenu", subTabs = {} },
+        { label = "SETTING", type = "subMenu", subTabs = {} }
     }
-end
-
-function PixelUI:UpdatePlayerTab()
-    local p = { { label = "Refresh Player List", type = "button", onSelect = function() PixelUI:UpdatePlayerTab() end } }
-    for _, player in ipairs(GetActivePlayers()) do
-        local sid = GetPlayerServerId(player)
-        local trolls = { 
-            { label = "Spectate", type = "toggle", value = false },
-            { label = "Teleport To", type = "button" },
-            { label = "Explode Player", type = "button", danger = true },
-            { label = "Freeze Player", type = "toggle", value = false }
-        }
-        table.insert(p, { label = GetPlayerName(player).." ["..sid.."]", type = "subMenu", subTabs = trolls })
-    end
-    ActiveMenu.content[1] = p
-    if ActiveMenu.activeTabIdx == 1 then
-        CurrentMenu = p
-        PixelUI:UpdateUI()
-    end
 end
 
 function PixelUI:UpdateUI()
     PixelUI:Send({
         action = "updateMenu",
         menuData = {
-            tabs = ActiveMenu.tabs,
-            activeTabIdx = ActiveMenu.activeTabIdx,
+            tabs = {"MAIN MENU"}, -- تبويب واحد فقط كعنوان
+            activeTabIdx = 0,
             items = CurrentMenu,
             selectedIndex = HoveredIndex - 1
         }
@@ -97,24 +52,14 @@ MachoOnKeyDown(function(key)
         elseif key == 40 then -- DOWN
             HoveredIndex = HoveredIndex < #CurrentMenu and HoveredIndex + 1 or 1
             PixelUI:Send({action="keydown", index=HoveredIndex-1})
-        elseif key == 37 or key == 39 then -- LEFT / RIGHT (Tabs)
-            if key == 37 then -- Left
-                ActiveMenu.activeTabIdx = ActiveMenu.activeTabIdx > 0 and ActiveMenu.activeTabIdx - 1 or #ActiveMenu.tabs - 1
-            else -- Right
-                ActiveMenu.activeTabIdx = ActiveMenu.activeTabIdx < #ActiveMenu.tabs - 1 and ActiveMenu.activeTabIdx + 1 or 0
-            end
-            CurrentMenu = ActiveMenu.content[ActiveMenu.activeTabIdx]
-            HoveredIndex = 1
-            MenuStack = {} -- Clear stack when changing tabs
-            PixelUI:UpdateUI()
         elseif key == 13 then -- ENTER
             local item = CurrentMenu[HoveredIndex]
-            if item.type == "subMenu" then
+            if item and item.type == "subMenu" then
                 table.insert(MenuStack, CurrentMenu)
                 CurrentMenu = item.subTabs
                 HoveredIndex = 1
                 PixelUI:UpdateUI()
-            elseif item.onSelect then 
+            elseif item and item.onSelect then 
                 item.onSelect() 
             end
         elseif key == 8 then -- BACKSPACE
@@ -138,9 +83,9 @@ function PixelUI:Init()
     DUI = MachoCreateDui(MENU_URL, 1920, 1080)
     MachoShowDui(DUI)
     PixelUI:Build()
-    CurrentMenu = ActiveMenu.content[0]
+    CurrentMenu = ActiveMenu
     Citizen.Wait(1000)
-    PixelUI:Notify("success", "PIXEL UI", "V2.5 Premium Loaded")
+    PixelUI:Notify("success", "PIXEL UI", "Menu Simplified")
 end
 
 PixelUI:Init()
