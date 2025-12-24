52779ed79aa1a93337ec5ff57a0ee830bd21d78e
dbd3mk <kklw654321@gmail.com>
2025-12-24 10:10:42 +0300

    Settings: Added menu position and key binding controls

--- PixelUI.lua
+++ PixelUI.lua
@@ -7,19 +7,39 @@ local BASE_URL = "https://dbd3mk.github.io/pxui-menu/"
 local MENU_URL = BASE_URL .. "index.html?v=" .. math.random(1, 999999)
 
 local MenuKey = 72 -- H Key
+local MenuPosX, MenuPosY = 30, 30
+local IsPickingKey, TempKey, TempName = false, nil, nil
 local isShiftPressed = false
+
+local Keys = {
+    [8]="BACK",[13]="ENTER",[27]="ESC",[32]=" ", [189]="_", [190]=".",
+    [48]="0",[49]="1",[50]="2",[51]="3",[52]="4",[53]="5",[54]="6",[55]="7",[56]="8",[57]="9",
+    [96]="0",[97]="1",[98]="2",[99]="3",[100]="4",[101]="5",[102]="6",[103]="7",[104]="8",[105]="9",
+    [65]="A",[66]="B",[67]="C",[68]="D",[69]="E",[70]="F",[71]="G",[72]="H",[73]="I",[74]="J",
+    [75]="K",[76]="L",[77]="M",[78]="N",[79]="O",[80]="P",[81]="Q",[82]="R",[83]="S",[84]="T",
+    [85]="U",[86]="V",[87]="W",[88]="X",[89]="Y",[90]="Z"
+}
 
 -- Core Functions
 function PixelUI:Send(d) if DUI then MachoSendDuiMessage(DUI, json.encode(d)) end end
 function PixelUI:Notify(t, ti, d) PixelUI:Send({ action="showNotification", type=t, title=ti, desc=d }) end
+function PixelUI:UpdatePos() PixelUI:Send({ action = "updatePosition", x = MenuPosX, y = MenuPosY }) end
 
 function PixelUI:Build()
-    -- القائمة الرئيسية البسيطة كما طلب المستخدم
+    local settingsMenu = {
+        { label = "Menu X Position", type = "list", value = MenuPosX, onSelect = function() end },
+        { label = "Menu Y Position", type = "list", value = MenuPosY, onSelect = function() end },
+        { label = "Change Toggle Key", type = "button", onSelect = function() 
+            IsPickingKey = true
+            PixelUI:Send({action="updateKeyboard", visible=true, title="Press New Key", value="???"})
+        end },
+    }
+
     ActiveMenu = {
         { label = "SELF", type = "subMenu", subTabs = {} },
         { label = "PLAYER", type = "subMenu", subTabs = {} },
         { label = "SERVER", type = "subMenu", subTabs = {} },
-        { label = "SETTING", type = "subMenu", subTabs = {} }
+        { label = "SETTING", type = "subMenu", subTabs = settingsMenu }
     }
 end
 
@@ -27,17 +47,59 @@ function PixelUI:UpdateUI()
     PixelUI:Send({
         action = "updateMenu",
         menuData = {
-            tabs = {"MAIN MENU"}, -- تبويب واحد فقط كعنوان
+            tabs = {"MAIN MENU"},
             activeTabIdx = 0,
             items = CurrentMenu,
             selectedIndex = HoveredIndex - 1
         }
     })
+    PixelUI:UpdatePos()
 end
+
+-- Thread for Position Adjustment
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(5)
+        if IsVisible then
+            local item = CurrentMenu[HoveredIndex]
+            if item and item.label then
+                if item.label:find("Position") then
+                    if IsDisabledControlPressed(0, 175) then -- Right
+                        if item.label:find("X") then MenuPosX = MenuPosX + 2 else MenuPosY = MenuPosY + 2 end
+                        item.value = item.label:find("X") and MenuPosX or MenuPosY
+                        PixelUI:UpdateUI()
+                    elseif IsDisabledControlPressed(0, 174) then -- Left
+                        if item.label:find("X") then MenuPosX = math.max(0, MenuPosX - 2) else MenuPosY = math.max(0, MenuPosY - 2) end
+                        item.value = item.label:find("X") and MenuPosX or MenuPosY
+                        PixelUI:UpdateUI()
+                    end
+                end
+            end
+        end
+    end
+end)
 
 -- Input Handling
 MachoOnKeyDown(function(key)
     if key == 16 or key == 160 or key == 161 then isShiftPressed = true return end
+
+    if IsPickingKey then
+        local char = Keys[key]
+        if char == "ENTER" and TempKey then 
+            MenuKey = TempKey
+            IsPickingKey = false
+            PixelUI:Send({action="updateKeyboard", visible=false})
+            PixelUI:Notify("success", "SETTINGS", "New Key Bound: " .. (TempName or "Unknown"))
+        elseif char == "BACK" then 
+            IsPickingKey = false
+            PixelUI:Send({action="updateKeyboard", visible=false})
+        else 
+            TempKey = key
+            TempName = char or "Key "..key
+            PixelUI:Send({action="updateKeyboard", visible=true, value=TempName})
+        end
+        return
+    end
 
     if key == MenuKey then
         IsVisible = not IsVisible
@@ -85,7 +147,7 @@ function PixelUI:Init()
     PixelUI:Build()
     CurrentMenu = ActiveMenu
     Citizen.Wait(1000)
-    PixelUI:Notify("success", "PIXEL UI", "Menu Simplified")
+    PixelUI:Notify("success", "PIXEL UI", "Settings Initialized")
 end
 
 PixelUI:Init()
